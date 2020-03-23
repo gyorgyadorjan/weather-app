eda8738528414fc12eb4e473648e92a3da70e924
Gyorgy Adorjan <aiden14080@gmail.com>
2020-03-23 00:15:50 +0000

    Replaced ip-api with built in geolocation

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
 async function main() {
     console.time("runtime");
 
-    // API call for geo location => [status, message, countryCode, city, lat, lon]
-    let ipAPI = await fetch("http://ip-api.com/json/?fields=49362");
-    let ipAPIJSON = await ipAPI.json();
-
-    // Error handling for geo location
-    if (ipAPIJSON.status === "fail") {
-        $(".error > p").append(`Error while requesting data from IP-API!<br/>Error message: ${ipAPIJSON.message}`);
+    // Check if Geolocation is not supported by the browser
+    if (!navigator.geolocation) {
+        $(".error > p").append("Geolocation is not supported by this browser.");
         $(".sk-chase").addClass("d-none");
         $(".error").removeClass("d-none");
         return;
     }
 
+    // Get geolocation
+    let position = await this.getCoordinates().catch((error) => {
+        // Error handling for geolocation
+        $(".error > p").append(`Error code ${error.code}. ${error.message}.`);
+        $(".sk-chase").addClass("d-none");
+        $(".error").removeClass("d-none");
+    });
+
+    // Stop execution if there was an error with geolocation
+    if (!position) return;
+
     // API call for weather data
-    let weatherAPI = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${ipAPIJSON.lat}&lon=${ipAPIJSON.lon}&units=metric&APPID=c10b45d9c21fc69d07bb0361ca76bd05`);
+    let weatherAPI = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&APPID=c10b45d9c21fc69d07bb0361ca76bd05`);
     let weatherAPIJSON = await weatherAPI.json();
 
     // Error handling for weather data
@@ -27,7 +34,7 @@ async function main() {
 
     // Extract the needed information
     let weather = {
-        "cityAndCountry": `${ipAPIJSON.city}, ${ipAPIJSON.countryCode}`,
+        "cityAndCountry": `${weatherAPIJSON.name}, ${weatherAPIJSON.sys.country}`,
         "temperature": Math.floor(weatherAPIJSON.main.temp),
         "temperatureDescription": weatherAPIJSON.weather[0].description,
         "iconSrc": `http://openweathermap.org/img/wn/${weatherAPIJSON.weather[0].icon}@2x.png`,
@@ -52,6 +59,13 @@ async function main() {
     });
 
     console.timeEnd("runtime");
+}
+
+// Geolocation promise function
+function getCoordinates() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    })
 }
 
 // start JS
